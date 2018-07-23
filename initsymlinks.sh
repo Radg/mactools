1cef21a799b1681200b2329d268bcacb467b42c2
Radg <radic@radic.ru>
2018-07-23 23:22:18 +0400

    Initial commit

---
+++ initsymlinks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Initial symlinks
+
+# airport utility
+sudo ln -s /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/local/bin/airport
+
+# Sublime as sl
+ln -s /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl /usr/local/bin/sublime
