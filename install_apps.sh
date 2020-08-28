4e39406c89a6b2d09240c83aa3ac1fc1c62a4c7c
Radg <radic@radic.ru>
2020-08-28 11:14:36 +0300

    Merge with macos-reinstall repository

---
+++ install_apps.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Install useful tools
+# brew should be installed first
+
+brew install ansible
+brew install mc
+brew install golang
+brew install speedtest-cli
+brew install zsh
+brew install rsync
+brew cask install iina
+brew cask install transmission
+brew cask install sublime-text
+brew cask install maccy
+brew cask install itsycal
+brew cask install firefox
+brew cask install iterm2
+brew cask install dozer
+brew cask install notion
+brew cask install mounty
+
