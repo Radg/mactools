a790dbdcd2b07b87b473b3f641ddff4ef85afac2
Radg <2058025+Radg@users.noreply.github.com>
2022-11-06 20:52:34 +0300

    brew packages and casks updated

--- install_apps.sh
+++ install_apps.sh
@@ -3,34 +3,10 @@
 # Install useful tools
 # brew should be installed first
 
-brew install ansible
-brew install mc
-brew install golang
-brew install speedtest-cli
-brew install zsh
-brew install vim
-brew install rsync
-brew install youtube-dl
-brew install nmap
-brew install tree
-brew install jq
-brew install grc
-brew install git-flow
-brew install watch
-brew install screen
-brew install --cask iina
-brew install --cask transmission
-brew install --cask sublime-text
-brew install --cask maccy
-brew install --cask itsycal
-brew install --cask firefox
-brew install --cask google-chrome
-brew install --cask iterm2
-brew install --cask dozer
-brew install --cask notion
-brew install --cask mounty
-brew install --cask mysqlworkbench
+# Install cli packages
 
-# Install zsh plugins
-git clone https://github.com/bobthecow/git-flow-completion ~/.oh-my-zsh/custom/plugins/git-flow-completion
+brew install $(cat brew_packages.txt)
 
+# Install casks
+
+brew install --cask $(cat brew_casks.txt)
